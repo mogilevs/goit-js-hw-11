fbd86733121439df7194ba8588a7637448663c13
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-19 21:39:28 +0300

    do fetch, markup, izitoast, css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,86 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import err from './img/err.png';
+
+const form = document.querySelector('.form');
+const list = document.querySelector('.list');
+form.addEventListener('submit', searchHandler);
+const key = '44447356-a60fa6f4c2d7f10e895940a18';
+
+function searchHandler(evt) {
+  list.innerHTML = '';
+  const text = evt.target.elements.input.value;
+  evt.preventDefault();
+  if (text !== '') {
+    fetch(
+      `https://pixabay.com/api/?key=${key}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const photos = data.hits;
+        if (photos.length !== 0) {
+          const markup = photos
+            .map(
+              ({
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+              }) => {
+                return `<li class="item">
+          <img class="image" src="${webformatURL}" alt="${tags}" />
+          <ul class="item-list">
+            <li class="item-list-item">
+              <h3 class="title">Likes</h3>
+              <p class="value">${likes}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Views</h3>
+              <p class="value">${views}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Comments</h3>
+              <p class="value">${comments}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Downloads</h3>
+              <p class="value">${downloads}</p>
+            </li>
+          </ul>
+        </li>`;
+              }
+            )
+            .join('');
+
+          list.insertAdjacentHTML('beforeend', markup);
+        } else {
+          iziToast.show({
+            class: 'toast',
+            position: 'topRight',
+            icon: 'icon',
+            iconUrl: err,
+            iconColor: 'white',
+            // backgroundColor: 'red',
+            // messageColor: 'white',
+            // messageSize: '16px',
+            // maxWidth: '432px',
+            message: `Sorry, there are no images matching your search query. Please try again!`,
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    form.reset();
+  }
+}
