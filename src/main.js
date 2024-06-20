e62de249d29df638eb9acaee7815e6a68d49eca8
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-06-20 18:50:13 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -3,20 +3,23 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import err from './img/err.png';
+import httpRequest from './js/pixabay-api.js';
+import createMarkup from './js/render-functions.js';
 
+const key = '44447356-a60fa6f4c2d7f10e895940a18';
 const form = document.querySelector('.form');
 const list = document.querySelector('.list');
+
 form.addEventListener('submit', searchHandler);
-const key = '44447356-a60fa6f4c2d7f10e895940a18';
 
 function searchHandler(evt) {
   list.innerHTML = '';
   const text = evt.target.elements.input.value;
   evt.preventDefault();
   if (text !== '') {
-    fetch(
-      `https://pixabay.com/api/?key=${key}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
-    )
+    form.insertAdjacentHTML('afterend', '<span class="loader"></span>');
+    const loader = document.querySelector('.loader');
+    httpRequest(key, text)
       .then(response => {
         if (!response.ok) {
           throw new Error(response.status);
@@ -26,50 +29,14 @@ function searchHandler(evt) {
       .then(data => {
         const photos = data.hits;
         if (photos.length !== 0) {
-          const markup = photos
-            .map(
-              ({
-                webformatURL,
-                largeImageURL,
-                tags,
-                likes,
-                views,
-                comments,
-                downloads,
-              }) => {
-                return `<li class="item">
-          <a href="${largeImageURL}"><img class="image" src="${webformatURL}" alt="${tags}" /></a>
-          <ul class="item-list">
-            <li class="item-list-item">
-              <h3 class="title">Likes</h3>
-              <p class="value">${likes}</p>
-            </li>
-            <li class="item-list-item">
-              <h3 class="title">Views</h3>
-              <p class="value">${views}</p>
-            </li>
-            <li class="item-list-item">
-              <h3 class="title">Comments</h3>
-              <p class="value">${comments}</p>
-            </li>
-            <li class="item-list-item">
-              <h3 class="title">Downloads</h3>
-              <p class="value">${downloads}</p>
-            </li>
-          </ul>
-        </li>`;
-              }
-            )
-            .join('');
-
-          list.insertAdjacentHTML('beforeend', markup);
+          list.insertAdjacentHTML('beforeend', createMarkup(photos));
           const lightbox = new SimpleLightbox('.list a', {
-	captions: true,
-	captionType: 'attr',
-	captionsData: 'alt',
-	captionPosition: 'bottom',
-	captionDelay: 250
-          }); 
+            captions: true,
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+          });
           lightbox.refresh();
         } else {
           iziToast.show({
@@ -85,9 +52,8 @@ function searchHandler(evt) {
       })
       .catch(error => {
         console.log(error);
-      });
+      })
+      .finally(() => (loader.style.display = 'none'));
     form.reset();
   }
 }
-
-
